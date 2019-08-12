a54384218a9012556796ffed3a7b1ea449ba4950
Bry Cox <carl.cc@gmail.com>
2019-08-12 07:08:26 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,10 +11,10 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
 
     if @author.valid?
-      @author.save
-      redirect_to author_path(@author)
+       @author.save
+       redirect_to author_path(@author)
     else
-      render :new
+       render :new
     end
   end
 
